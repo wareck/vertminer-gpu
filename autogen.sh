4612289c6fa10ce70e9d971e1f8d5cc011ee9f20
Your Name <you@example.com>
2017-04-10 08:31:47 +0200

    no auto make

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ bs_dir="$(dirname $(readlink -f $0))"
 touch "${bs_dir}"/ltmain.sh
 autoreconf -fi "${bs_dir}"
 
-if test -z "$NOCONFIGURE" ; then
-	echo 'Configuring and build...'
-	"$bs_dir"/buildit.sh"$@"
-fi
+#if test -z "$NOCONFIGURE" ; then
+#	echo 'Configuring and build...'
+#	"$bs_dir"/buildit.sh"$@"
+#fi
